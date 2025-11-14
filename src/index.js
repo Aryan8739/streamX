98019d2fe21c090e814d82d9a6235ff3347f5056
Aryan Rastogi <aryan.rastogi.20nov@gmail.com>
2025-11-14 20:46:00 +0530

    DB conn and dependencies

--- src/index.js
+++ src/index.js
@@ -1,2 +1,34 @@
-import mongoose from "mongoose";
+// require('dotenv').config( {path: './env'})
+import 'dotenv/config';
 
+
+import connectDB from "./db/conn.js";
+
+// dotenv.config({
+//     path: './env'
+// }) 
+
+
+connectDB()
+
+
+/*
+import express from "express";
+const app=express();
+
+;(  async ( ) => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+        app.on("error", (error)=> {
+            console.log(("error: ", error));
+            throw error
+            
+        })
+    } catch (error) {
+        console.error("Error connecting to the database:", error);
+        throw error;
+        
+    }
+}) ( )
+    */
+   
