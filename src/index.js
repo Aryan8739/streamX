aee3343aed62b8be7b5fb0e6bc3ae0421fb1c297
Aryan Rastogi <aryan.rastogi.20nov@gmail.com>
2025-11-15 21:01:58 +0530

    Connect return promise

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@
 import 'dotenv/config';
 
 
+import express from "express";
 import connectDB from "./db/conn.js";
+
+const app = express();
 
 // dotenv.config({
 //     path: './env'
@@ -10,6 +13,17 @@ import connectDB from "./db/conn.js";
 
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is Running at PORT ${process.env.PORT || 8000}`);
+        
+    });
+})
+.catch((error)=> {
+    console.log("MONGO DB Connection failed;",error);
+    
+})
+
 
 
 /*
