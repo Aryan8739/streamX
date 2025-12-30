2f0c8f0dd392f3f23f0a82b5517ae32e52751908
Aryan Rastogi <aryan.rastogi.20nov@gmail.com>
2025-12-30 21:00:07 +0530

    access and refresh token

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -39,7 +39,7 @@ const  userSchema   = new  Schema(
             }
         ],
 
-        passoword: {
+        password: {
             type: String ,
             required : [true,"Password is required"]
         },
@@ -50,6 +50,46 @@ const  userSchema   = new  Schema(
     {
         timestamps: true,
 });
+
+userSchema.pre("save",  async function (next) {
+    if(!this.isModified("password")) return next();
+    this.password =  bcrypt.hash(this.password,10);
+    next();
+})
+
+userSchema.methods.isPasswordValid = async function (password) {
+    return await bcrypt.compare(password,this.password);
+}
+
+userSchema.methods.generateAccessToken = function () {
+    JsonWebTokenError.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullname: this.fullname,
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_LIFE
+        }
+    )
+}
+userSchema.methods.generateRefreshToken = function () {
+    JsonWebTokenError.sign(
+        {
+            _id: this._id,
+            
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_LIFE
+        }
+    )
+}
+userSchema.methods.generateRefreshToken = function () {}
+
+
 
 export const User = mongoose.model("User", userSchema);
 
