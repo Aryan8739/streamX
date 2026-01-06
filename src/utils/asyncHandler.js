9c6abaecf7b94c0de6bca69a5290015d30283e5c
Aryan Rastogi <aryan.rastogi.20nov@gmail.com>
2026-01-07 00:31:57 +0530

    Async Handler Using Promise and TryCatch

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,5 +1,15 @@
-const asyncHandler = () => {}
-
+// using promise.catch
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next))
+        .catch((err) => {
+            res.status(err.code || 500).json({
+                success: false,
+                message: err.message || 'Internal Server Error'
+            });
+        });
+}
+}
 
 
 export {asyncHandler }
@@ -10,4 +20,15 @@ export {asyncHandler }
 // const asyncHandler= (fn) => () => {}
 // const asyncHandler = (fn) => asyncHandler() => {}
 
-    
+
+    // Using try catch
+    // const asyncHandler = (fn) => async (req, res, next) => {
+    //     try { 
+    //         await fn(req, res, next);
+    //     }
+    //     catch(err) {
+    //         res.status(err.code || 500).json({
+    //             sucess: false,
+    //             message: err.message || 'Internal Server Error'});
+    //     }
+    // }
