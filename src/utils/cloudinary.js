aefbab8cad3485cf6dd318e616735409e7dcea32
Aryan Rastogi <aryan.rastogi.20nov@gmail.com>
2026-01-15 16:48:37 +0530

    Cloudinaru util

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,33 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs from "fs";
+
+    // Configuration
+ cloudinary.config({ 
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME , 
+        api_key: process.env.CLOUDINARY_API_KEY , 
+        api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const uploadOnCloudinary= async (filePath ) => {
+    try {
+        if (!filePath) return null;
+        //upload the file on CLoudinary
+        const response = await cloudinary.uploader.upload(filePath , {
+            resource_type : "auto",
+        })
+        console.log("File Uploaded", response.url);
+        return response
+        }
+        catch (error) { 
+            fs.unlinkSync(filePath); //remove the file from local saved temp file as upload ooperation got failed.
+            return null;
+        }
+    }
+
+    export { uploadOnCloudinary };
+
+
+
+cloudinary.v2.uploader.upload("https://upload.wikimedia.orgorg/wikipedia/commons/a/ae/Olympic_flag.jpg" ,
+{ public_id: "olympic_flag" },
+function(error, result) { console.log(result); });
