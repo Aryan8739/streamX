82364268f4e074bacc701be7df638e2b4afaf64b
Aryan Rastogi <aryan.rastogi.20nov@gmail.com>
2026-02-12 17:41:04 +0530

    db name in conn from const

--- src/db/conn.js
+++ src/db/conn.js
@@ -5,7 +5,8 @@ const connectDB = async ( ) =>{
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL }/${DB_NAME}`)
         console.log(`\n MongoDB Connected Successfully!! DB HOST: ${connectionInstance.connection.host}`);
-        
+        console.log("DB NAME:", connectionInstance.connection.name);
+ 
     }
     catch (error) {
         console.log("MongoDB connection error",error);
