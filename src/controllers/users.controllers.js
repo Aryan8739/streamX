fe174b142da292ff5008247ed39ae149f01997f3
Aryan Rastogi <aryan.rastogi.20nov@gmail.com>
2026-03-02 00:37:58 +0530

    algo steps for registration

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -1,10 +1,21 @@
-import {asyncHandler} from "../Backend/src/utils/asyncHandler.js"
-
+import {asyncHandler} from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler(async (req,res)=> {
-  res.status(200).json({
-    message: "ok"
-  })
+  //get user detail from frontend
+  //validation - not empty
+  //check if already exist username,email
+  //check for images , for avtar
+  //upload them to cloudinary
+  //check in cloudinary
+  //create user object - create entry in db
+  //remove pass and refresh token from res
+  //check for user
+  //return res
+
+
+
+  const{fullName,email,username,password}= req.body
+  console.log("email",email);
 }) 
 
 export {registerUser}
