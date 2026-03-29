770a25e246a341f64b4b6a17c5658d4559c5336b
Aryan Rastogi <aryan.rastogi.20nov@gmail.com>
2026-03-30 05:14:22 +0530

    without coverimage

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -8,12 +8,16 @@ import fs from "fs";
 
 const registerUser = asyncHandler(async (req, res) => {
 
-  const { fullName, email, username, password } = req.body || {}; 
+  const { fullName, email, username, password } = req.body || {};
   console.log("email:", email);
 
   // ✅ Validation (Move file paths up for cleanup)
   const avatarLocalPath = req.files?.avatar?.[0]?.path;
-  const coverImageLocalPath = req.files?.coverImage?.[0]?.path;
+  // const coverImageLocalPath = req.files?.coverImage?.[0]?.path;
+  let coverImageLocalPath;
+  if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+    coverImageLocalPath = req.files.coverImage[0].path
+  }
 
   if (
     [fullName, email, username, password].some(
