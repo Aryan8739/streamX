721fea8a6ca509ac5b87454aadbca7fe90912e48
Aryan Rastogi <aryan.rastogi.20nov@gmail.com>
2026-04-04 03:20:36 +0530

    typos

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -269,7 +269,7 @@ const changeCurrentPassword = asyncHandler(async(req,res)=>{
 const getCurrentUser = asyncHandler(async(req,res)=>{
   return res
   .status(200)
-  .json(200,req.user, "Current user fetched Successfully")
+  .json(new ApiResponse(200,req.user, "Current user fetched Successfully"))
 
 })
 
@@ -281,8 +281,8 @@ const updateAccountDetails = asyncHandler(async(req,res)=>{
 
   }
 
-  const user = User.findByIdAndUpdate(
-    req.user?.id,
+  const user = await User.findByIdAndUpdate(
+    req.user?._id,
     {
       $set: {
         fullName,
