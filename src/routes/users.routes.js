5febcbb2cc5290cbc49c00fedc1559af28ae1059
Aryan Rastogi <aryan.rastogi.20nov@gmail.com>
2026-04-05 00:10:18 +0530

    added all ROUTES for Controllers

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -5,7 +5,13 @@ import {upload} from "../middlewares/multer.middlewares.js";
 import { logoutUser } from "../controllers/users.controllers.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import {refreshAccessToken } from "../controllers/users.controllers.js";
-
+import {changeCurrentPassword  } from "../controllers/users.controllers.js";
+import {getCurrentUser } from "../controllers/users.controllers.js";
+import {updateAccountDetails } from "../controllers/users.controllers.js";
+import {updateUserAvatar } from "../controllers/users.controllers.js";
+import {updateUserCoverImage } from "../controllers/users.controllers.js";
+import {getUserChannelProfile } from "../controllers/users.controllers.js";
+import {getWatchHistory } from "../controllers/users.controllers.js";
 
 
 const router = Router()
@@ -25,6 +31,13 @@ router.route("/login").post(loginUser)
 //Secure Routes
 router.route("/logout").post(verifyJWT , logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJWT,changeCurrentPassword)
+router.route("/current-user").get(verifyJWT,getCurrentUser)
+router.route("/update-account").patch(verifyJWT,updateAccountDetails)
+router.router("/avatar").patch(verifyJWT,upload.single("avatar"),updateUserAvatar)
+router.route("cover-image").patch(verifyJWT,upload.single("coverImage"),updateUserCoverImage)
+router.route("/c/:username").get(verifyJWT,getUserChannelProfile )
+router.route("/history").get(verifyJWT,getWatchHistory)
 
 
 export default router
