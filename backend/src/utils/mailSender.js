8b5cdfec74e59b11ba0313342cbcefdf8ebb2258
Aryan Rastogi <aryan.rastogi.20nov@gmail.com>
2026-04-29 20:47:15 +0530

    otp bug fixed shayad

--- backend/src/utils/mailSender.js
+++ backend/src/utils/mailSender.js
@@ -5,10 +5,15 @@ const mailSender = async (email, title, body) => {
     try {
         let transporter = nodemailer.createTransport({
             host: process.env.MAIL_HOST,
+            port: 465,
+            secure: true,
             auth: {
                 user: process.env.MAIL_USER,
                 pass: process.env.MAIL_PASS,
-            }
+            },
+            connectionTimeout: 10000, // 10 seconds
+            greetingTimeout: 10000,
+            socketTimeout: 10000
         });
 
         let info = await transporter.sendMail({
